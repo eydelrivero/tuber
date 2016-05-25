868db971369b169245960961b1b16b966ef160a5
eydelrivero <eydel.rivero@gmail.com>
2016-05-25 18:48:54 +0200

    use ldply instead

--- R/yt_search.R
+++ R/yt_search.R
@@ -58,7 +58,8 @@ yt_search <- function (term=NULL, maxResults=5, channelId= NULL, channelType=NUL
 
 	if (res$pageInfo$totalResults != 0) {
 		simple_res  <- lapply(res$items, function(x) c(x$id$videoId, x$snippet))
-		resdf       <- as.data.frame(do.call(rbind, simple_res))
+		# resdf       <- as.data.frame(do.call(rbind, simple_res))
+		resdf       <- ldply(simple_res, data.frame)
 		colnames(resdf)[1] <- "id"
 	} else {
 		resdf <- 0
